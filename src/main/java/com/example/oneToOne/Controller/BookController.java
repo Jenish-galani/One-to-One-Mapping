df97d3f91c16096ae17cb2149ad105ff59da099e
Jenish Galani <jenishgalani4543@gmail.com>
2024-12-30 12:19:41 +0530

    Update BookController.java

--- src/main/java/com/example/oneToOne/Controller/BookController.java
+++ src/main/java/com/example/oneToOne/Controller/BookController.java
@@ -13,29 +13,34 @@ public class BookController {
     @Autowired
     private BookServ serv;
 
+    //Use for insert record
     @PostMapping()
     public String save(@RequestBody Book book) {
         serv.save(book);
         return "Record successfully inserted";
     }
 
+    //Use for specific record update by id
     @PutMapping("id/{myId}")
     public String updateRecord(@RequestBody Book book){
         serv.save(book);
         return "Record successfully updated";
     }
 
+    //Use for specific record delete by id
     @DeleteMapping("id/{myId}")
     public String deleteById(@PathVariable Long myId){
         serv.deleteOne(myId);
         return "Record successfully deleted";
     }
 
+    //Use for specific record get by id
     @GetMapping("id/{myId}")
     public Optional<Book> getOne(@PathVariable Long myId) {
         return serv.getOne(myId);
     }
 
+    //Use for all records get
     @GetMapping()
     public List<Book> getAll(){
         return serv.getAll();
