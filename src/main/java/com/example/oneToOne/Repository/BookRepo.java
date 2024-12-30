aa469224e6d3f473e1c81d82095b9722b012816e
Jenish Galani <jenishgalani4543@gmail.com>
2024-12-30 12:17:14 +0530

    Update BookRepo.java

--- src/main/java/com/example/oneToOne/Repository/BookRepo.java
+++ src/main/java/com/example/oneToOne/Repository/BookRepo.java
@@ -3,5 +3,6 @@ package com.example.oneToOne.Repository;
 import com.example.oneToOne.Models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface BookRepo extends JpaRepository<Book,Long> {
 }
